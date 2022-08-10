9340657e0e4fc9de3c5691787ac9d5894c0d31bb
tiendatmagic <tiendatmagic8@gmail.com>
2022-08-10 22:55:02 +0700

    update

---
+++ market/js/script.js
@@ -0,0 +1,42 @@
+const formatter = new Intl.NumberFormat("en-US", {
+    currency: "VND",
+    minimumFractionDigits: 0,
+});
+const container = document.querySelector(".container");
+
+function update() {
+    var datac = "";
+    fetch("https://exchange.vndc.io/exchange/api/v1/showup-prices")
+        .then((res) => res.json())
+        .then((json) => {
+            const coins = Object.getOwnPropertyNames(json);
+
+            for (let coin of coins) {
+                if (coin.substr(coin.length - 4) == "VNDC") {
+                    const coinInfo = json[`${coin}`];
+                    const price = formatter.format(coinInfo.bid);
+                    const currency_unit = coin.substr(coin.length - 4);
+
+                    datac += `
+                <div class="coin rising">
+                    <div class="coin-logo">
+                    </div>
+                    <div class="coin-name">
+                        <h3>${coin.slice(0, coin.length - 4)}</h3>
+                        <span>/${currency_unit}</span>
+                    </div>
+                    <div class="coin-price">
+                        <span class="price">$${price}</span>
+
+                    </div>
+                </div>
+            `;
+                }
+            }
+            container.innerHTML = datac;
+        })
+        .catch(function (error) {
+            console.log("Request failed", error);
+        });
+}
+setInterval(update, 1000);
